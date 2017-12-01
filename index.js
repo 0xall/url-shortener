54c4de8af982b7f8e74b7e5a5edf05c98dda2017
seungwon-kang <ksw3894@gmail.com>
2017-12-02 01:27:40 +0900

    Add an explaination comment in the index.js file

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
+/**
+ * URL shortener
+ * Created by Seungwon Kang
+ * 
+ * URL shortener produces short URLs for long URLs like
+ * http://.../.../ => http://[hostname]/AbCdEf
+ * 
+ * It uses base64 encoding for creating URL parameter. 
+ */
+
 var http = require('http');
 var app = require('./app');
 
